d5007add408e7d5ca1741a0b50ed8abd88d40c9a
Jeremias G. Sanchez <Jeremias@gmail.com>
2023-01-18 19:54:14 -0300

    primera parte del navbar terminado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import NavBar from "./component/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <NavBar/>
+    </>
   );
 }
-
 export default App;
---
+++ src/component/BotonMenu.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../Style/botonMenu.css"
+
+function BotonMenu(props) {
+  
+  return (
+    <div onClick={props.handleClicked}  className={`icon nav-icon-5 ${props.clicked ? 'open' : ''}`}>
+    <span></span>
+    <span></span>
+    <span></span>
+  </div>
+  )
+}
+
+export default BotonMenu
